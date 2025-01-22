3c323cb5a22e30bd7de593303e6c17c3af5b5105
Gaurav kumar <indiagauravkumar@gmail.com>
2025-01-23 03:15:36 +0530

    add todo routes for fetching todos

---
+++ be/routes/todo.routes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+import {getTodos} from '../controllers/todo.controller.js'
+
+
+const router = express.Router();
+
+router.get('/',getTodos);
+// router.post('/',createTodo);
+// router.put('/:id',updateTodo);
+// router.delete('/:id',deleteTodo);
+
+export default router;
