80a622ee361f6bc8a21a9af5918f3704c078dedf
Gaurav kumar <indiagauravkumar@gmail.com>
2025-01-24 01:40:44 +0530

    add button to navigate to CreatePage from HomePage

--- fe/src/pages/HomePage.jsx
+++ fe/src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import { useEffect } from "react";
 import { useProductStore } from "../store/product";
 import ProductCard from "../components/ProductCard";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
   const { products, fetchProducts } = useProductStore();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+
+    navigate("/create")
+    
+  }
 
   useEffect(() => {
     fetchProducts();
@@ -13,8 +21,14 @@ const HomePage = () => {
 
   return (
     <>
+    <div>
+
+        <button onClick={handleClick} >Add Product</button>
+
+    </div>
       {products.map((product) => (
         <ProductCard
+         id={product.id}
           key={product.id}
           name={product.name}
           price={product.price}
