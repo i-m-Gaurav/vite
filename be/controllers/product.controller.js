559276248ab7fd783f0fd6fac22667f2c11e97e1
Gaurav kumar <indiagauravkumar@gmail.com>
2025-01-24 01:40:14 +0530

    add product management functions; implement deleteProduct and updateProduct in product controller

--- be/controllers/product.controller.js
+++ be/controllers/product.controller.js
@@ -2,6 +2,9 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// To get all the products
+
+
 export const getProducts = async (req, res) => {
   try {
     const product = await prisma.product.findMany();
@@ -12,6 +15,7 @@ export const getProducts = async (req, res) => {
 };
 
 
+// To create the product.
 export const createProduct = async (req, res) => {
   const { name, description, image, price, quantity } = req.body;
 
@@ -41,3 +45,54 @@ export const createProduct = async (req, res) => {
       .json({ success: false, message: "Failed to create product" });
   }
 };
+
+// To delete a single product.
+
+export const deleteProduct = async (req,res) => {
+
+    const {id} = req.params;
+    console.log(req.params);
+
+    try {
+
+        await prisma.product.delete({
+            where : {
+                id : id,
+            }
+        })
+
+        res.status(200).json({success : true, message : "Product Deleted"});
+
+
+        
+    } catch (error) {
+
+        console.error("Failed to delete", error);
+        res.status(500).json({success : false, message : "Failed to delete the product"});
+        
+    }
+
+}
+
+export const updateProduct = async (req,res) => {
+
+    const { id } = req.params;
+
+    const  { name, price, description, image, quantity} = req.body;
+
+    
+
+    try {
+        
+        const updatedProduct = await prisma.product.update({
+            where: { id: id },
+            data: { name, price, description, image , quantity}
+        });
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+
+        console.error("Failed to update the product ", error);
+        
+    }
+
+}
